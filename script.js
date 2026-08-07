0105a6349d344c6a4036968cb55d66959b8d2662
paulo620 <peedesantos40@gmail.com>
2026-08-07 20:46:09 -0300

    Foi adicionado mas código no script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ console.log ("Hello word")
 function sum(a, b) {
     return a + b
 }
+function sub(a, b) {
+    return a -b 
+}
+function mul(a, b) {
+    return a * b
+}
+
+console.log (sum(2, 3))
+console.log (sub(5, 2))
+console.log (mul(3, 4))
